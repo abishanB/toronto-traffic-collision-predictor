ed718a79e316619187716880e2af37c66346e05a
abishanB <abishan.bhavan@gmail.com>
2025-10-18 22:55:53 -0400

    create next project

--- KernalDensityModel/KernalDensity.py
+++ KernalDensityModel/KernalDensity.py
@@ -3,10 +3,7 @@ import pandas as pd
 from sklearn.neighbors import KernelDensity
 import folium
 
-# Center map roughly on Toronto
 m = folium.Map(location=[43.7, -79.4], zoom_start=12)
-
-# Add a click listener to show coordinates
 m.add_child(folium.LatLngPopup())
 
 m
