d2fbdfb3a489be307306fd551afc6845a221b306
abishanB <abishan.bhavan@gmail.com>
2025-10-26 00:05:00 -0400

    add vehicle type, driver action, impact type inputs

--- RandomForestModel/testFeatures.py
+++ RandomForestModel/testFeatures.py
@@ -6,8 +6,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("./RandomForestModel/ksi_collisions.csv")
-categorical_features = ['LIGHT', 'VISIBILITY',
-                        'ROAD_CONDITION', 'DOW', 'TIME_OF_DAY', 'SEASON']
+categorical_features = ['LIGHT', 'VISIBILITY', 'ROAD_CONDITION', 'DOW',
+                        'TIME_OF_DAY', 'SEASON', 'VEHICLE_TYPE', 'DRIVER_ACTION', 'IMPACT_TYPE']
 encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
 encoded_array = encoder.fit_transform(df[categorical_features])
 encoded_cols = encoder.get_feature_names_out(categorical_features)
@@ -18,11 +18,11 @@ y = df['SEVERE_COLLISION']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42)
 params = {
-    'n_estimators': list(range(8, 22, 2)),
-    'max_depth': list(range(4, 22, 2)),
+    'n_estimators': list(range(10, 70, 10)),
+    'max_depth': list(range(4, 20, 2)),
     'min_samples_split': [2],
     'min_samples_leaf': [1],
-    'max_features': [None]
+    'max_features': ['sqrt', None]
 }
 
 rf = RandomForestClassifier(random_state=42)
